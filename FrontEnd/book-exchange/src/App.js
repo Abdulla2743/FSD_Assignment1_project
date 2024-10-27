1778b0deb62f310135b8eeb452216eab5fe4a756
adilgma <131982840+jamshed-adil@users.noreply.github.com>
2024-10-27 22:04:36 +0530

    commit on successfully getting data

--- FrontEnd/book-exchange/src/App.js
+++ FrontEnd/book-exchange/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './Components/Register';
 import Login from './Components/Login';
 import Home from './Components/Home';
+import GetBook from './Components/GetBook';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                     <Route path="/add-book" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
+                    <Route path="/get-books" element={<GetBook />} />
                 </Routes>
             </div>
         </Router>
