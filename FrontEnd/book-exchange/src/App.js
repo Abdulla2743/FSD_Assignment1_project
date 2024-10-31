acef5a8b63f13fe2c7900ca572cec1e357406eb7
Shaik Abdulla <Shaikabdulla2743@gmail.com>
2024-10-31 10:34:38 +0530

    update in register page

--- FrontEnd/book-exchange/src/App.js
+++ FrontEnd/book-exchange/src/App.js
@@ -4,12 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './Components/Register';
 import Login from './Components/Login';
 import Home from './Components/Home';
-<<<<<<< HEAD
 import HomePage from './Components/HomePage';
 import Admin from './Components/Admin';
-=======
-import GetBook from './Components/GetBook';
->>>>>>> 1778b0deb62f310135b8eeb452216eab5fe4a756
 
 function App() {
     return (
@@ -19,13 +15,9 @@ function App() {
                     <Route path="/add-book" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
-<<<<<<< HEAD
                     <Route path="/home" element={<HomePage />} />
                     <Route path="/admin" element={<Admin />} />
 
-=======
-                    <Route path="/get-books" element={<GetBook />} />
->>>>>>> 1778b0deb62f310135b8eeb452216eab5fe4a756
                 </Routes>
             </div>
         </Router>
