615729c3ca48fef9fa41226dc9f3eb9bf5073b40
Shaik <Shaikabdulla2743@gmail.com>
2024-10-31 09:45:14 +0530

    API routes and forms

--- FrontEnd/book-exchange/src/App.js
+++ FrontEnd/book-exchange/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './Components/Register';
 import Login from './Components/Login';
 import Home from './Components/Home';
+import HomePage from './Components/HomePage';
+import Admin from './Components/Admin';
 
 function App() {
     return (
@@ -13,6 +15,9 @@ function App() {
                     <Route path="/add-book" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
+                    <Route path="/home" element={<HomePage />} />
+                    <Route path="/admin" element={<Admin />} />
+
                 </Routes>
             </div>
         </Router>
--- FrontEnd/book-exchange/src/Components/Register.js
+++ FrontEnd/book-exchange/src/Components/Register.js
@@ -19,11 +19,13 @@ function Register() {
     };
 
     return (
+        <>
         <form onSubmit={handleRegister}>
             <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
             <button type="submit">Register</button>
         </form>
+        </>
     );
 }
 
